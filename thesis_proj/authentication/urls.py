5d7d5a01fad6354124d0859ff776845177c4402c
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2023-04-26 09:50:46 +0800

    test and configure login

---
+++ thesis_proj/authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('homepage', views.homepage, name='homepage'),
+]
---
+++ thesis_proj/authentication/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+@login_required(login_url='/login')
+def homepage(request):
+    return render(request, 'authentication/homepage.html')
